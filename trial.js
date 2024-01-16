46b8a6ea2217d445ed524b6644a344fa3995b440
ANKIT SINGH <dharmendarsingh1378@gmail.com>
2024-01-16 17:00:53 +0530

    first commit

--- trial.js
+++ trial.js
@@ -1,1 +1,2 @@
-console.log("ins cheetah,ins guldar,ins kambhir asw corvette ins kiltan,ins khukri,ins kavaratti,ins kamorta ins ranvijay r class);
+console.log("ins cheetah,ins guldar,ins kambhir asw corvette ins kiltan,ins khukri,ins kavaratti,ins kamorta ins ranvijay r class)
+console.log("always have a achievable goal");
