f5769c5578fb5bb3dd932fb02d77b51e08326c1e
ANKIT SINGH <dharmendarsingh1378@gmail.com>
2024-01-16 13:21:02 +0530

    first commit

--- trial.js
+++ trial.js
@@ -1,1 +1,1 @@
-console.log("ins cheetah,ins guldar,ins kambhir asw corvette ins kiltan,ins khukri,ins kavaratti,ins kamorta);
+console.log("ins cheetah,ins guldar,ins kambhir asw corvette ins kiltan,ins khukri,ins kavaratti,ins kamorta ins ranvijay r class);
