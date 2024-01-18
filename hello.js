0fc8874333d471431ab4d59ea9eb83d19461e52a
ANKIT SINGH <dharmendarsingh1378@gmail.com>
2024-01-18 12:44:06 +0530

    first commit

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
 console.log("Ankit is a commissioned officer in indian navy")
 console.log("ANKIT WILL GET WHATEVER HE NEEDS IN HIS LIFE")
-console.log("EAST OR WEST ANKIT SINGH IS THE BEST HE IS LIVI;NG  A LIFE KING SIZE");
+console.log("EAST OR WEST ANKIT SINGH IS THE BEST HE IS LIVI;NG  A LIFE KING SIZEE");
