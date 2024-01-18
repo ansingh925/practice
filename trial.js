70feb7126552ab49aa020605592cb1b8416351b1
ANKIT SINGH <dharmendarsingh1378@gmail.com>
2024-01-18 13:15:37 +0530

    add a new quote

--- trial.js
+++ trial.js
@@ -1,2 +1,3 @@
 console.log("ins cheetah,ins guldar,ins kambhir asw corvette ins kiltan,ins khukri,ins kavaratti,ins kamorta ins ranvijay r class)
 console.log("always have a achievable goal");
+console.log("today is my day");
